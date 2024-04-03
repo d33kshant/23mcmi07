18cb8be71ce274fb13760ca5f07b07f8be146333
Deekshant Yadav <68581632+d33kshant@users.noreply.github.com>
2024-04-03 04:30:24 +0000

    feat: added withBaseURL filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
+const BASE_URL = "https://obscure-space-eureka-j995x94ggwv2pw4j-8080.app.github.dev" // "https://scis.uohyd.ac.in/~23mcmi07"
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addFilter("withBaseURL", function (url="") {
+    try {
+      return new URL(BASE_URL + url).href;
+    } catch (err) {
+      console.error(err);
+      return url;
+    }
+  });
 };
